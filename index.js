3ff30fb6ebaf18d8d405826be38b4c0fd42a97c3
Denys Shpak <mr.shpak@gmail.com>
2024-06-13 16:10:43 +0300

    initial

---
+++ index.js
@@ -0,0 +1,32 @@
+import express from 'express';
+import cookieParser from 'cookie-parser';
+import cors from 'cors';
+
+const app = express();
+const router = express.Router();
+
+router.get('/set-cookie', (_, res) => {
+  res.cookie('testcookie', 'cookie-body', {
+    httpOnly: true, // Захищає куку від доступу через JavaScript
+    secure: true, // Дозволяє передачу куки лише через HTTPS
+    sameSite: 'None', // Дозволяє передачу куки в межсайтовому контексті
+  });
+  res.send('Cookie set');
+});
+
+router.get('/get-cookie', (req, res) => {
+  const myCookie = req.cookies.testcookie;
+  if (myCookie) {
+    res.send(`Cookie value: ${myCookie}`);
+  } else {
+    res.send('Cookie was not found');
+  }
+});
+
+app.use(cors());
+app.use(cookieParser());
+app.use('/', router);
+
+app.listen(3000, () => {
+  console.log('Server running on port 3000');
+});
